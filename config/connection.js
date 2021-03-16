84b93032d88eee8329a658c66ec520f1a9effbc8
vanesa gonzalez <gvane1919@gmail.com>
2021-03-16 17:26:03 -0400

    started the build up of the homework

---
+++ config/connection.js
@@ -0,0 +1,5 @@
+//do i need sequelizer for this HW???
+//const Sequelize = require('sequelize');
+
+// Enable access to .env variables
+require('dotenv').config();
---
+++ config/db/schemas.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS work_db;
+CREATE DATABASE work_db;
