e3bd27c07081cb05031c532e7049687d0bd7157d
vanesa gonzalez <gvane1919@gmail.com>
2021-03-20 01:27:32 -0400

    started connection.js file

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,49 @@
 const Sequelize = require('sequelize');
+const inquirer = require('inquirer');
+const mysql = require('mysql')
 
 // Enable access to .env variables
 require('dotenv').config();
+
+
+ //creating the mysql connections
+const connection = mysql.createConnection({
+port: 3306,
+user: 'root',
+password: Pepe1009,
+database: 'work_db',
+});
+//creating the IF error 
+connection.connect((err)=>{
+    if (err) thow err;
+    runSearch();
+});
+
+// use inquire to do the questions 
+const runSearch = () =>{
+inquirer
+.prompt({
+    name:'action',
+    type:'list',
+    message:'What would you like to do?',
+    Choices: [
+        'View all Employees', 
+        'View all Employees by departments',
+        'View all employees by manager',
+  //do i add the add/remove/update role/ update management here??
+
+    ]
+})
+.then((answer)=> {
+switch (answer.action){
+    case'View all Employees'
+}
+
+
+
+}
+)
+
+
+} 
+
